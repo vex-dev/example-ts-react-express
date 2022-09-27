2a3c22f8b978c0a3e8ede1ac525249e4cadb7f9d
Sam Pierson <sam@vex.dev>
2022-09-26 17:16:31 -0800

    client: App component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,66 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Vex, VexConnection} from '@vex.dev/web-sdk';
+import VideoRoom from './VideoRoom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface AppProps {
+}
+
+interface AppState {
+  connectionStatus: string;
+  vexInstance?: Vex
+  conn?: any,
+  roomId: string
+}
+
+class App extends React.Component<AppProps, AppState> {
+
+  constructor(props: AppProps) {
+    super(props);
+    this.state = {
+      connectionStatus: "disconnected",
+      roomId: window.vex_room_id
+    };
+  }
+
+  componentDidMount() {
+    this.connectToVex();
+  }
+
+  connectToVex() {
+    const vex = new Vex({
+      url: 'wss://app.vex.dev',
+      onDisconnect: () => {
+        this.onVexDisconnect()
+      }
+    });
+    this.setState({vexInstance: vex, connectionStatus: "connecting..."});
+    vex.connect().then(conn => {
+      this.onVexConnect(conn)
+    });
+  }
+
+  onVexConnect(conn: VexConnection) {
+    this.setState({connectionStatus: "connected", conn})
+  }
+
+  onVexDisconnect() {
+    this.setState({connectionStatus: "disconnected"})
+  }
+
+  render() {
+    return (
+      <div className='video-app'>
+        <p>Vex: {this.state.connectionStatus}</p>
+        {this.state.connectionStatus === "connected" &&
+        <VideoRoom roomId={this.state.roomId}
+                   // @ts-ignore
+                   vex={this.state.vexInstance}
+                   conn={this.state.conn}/>
+        }
+      </div>
+    )
+  }
 }
 
 export default App;
